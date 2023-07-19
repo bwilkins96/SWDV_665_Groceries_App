5d5b804c60605a50546af4f7d8e25cc83fb24175
bwilkins96 <ben.jr.speed@gmail.com>
2023-07-19 15:06:26 -0400

    edit grocery item

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -48,6 +48,29 @@ export class Tab1Page {
     }
   }
 
+  async presentEditBox(item: GroceryItem) {
+    const inputs = [
+      {placeholder: 'Item Name', value: item.name},
+      {placeholder: 'Quantity', value: item.quantity}
+    ]
+
+    const alert = await this.alertController.create({
+      header: 'Edit Grocery Item',
+      inputs: inputs,
+      buttons: ['Update Item']
+    });
+
+    await alert.present();
+
+    try {
+      const data = await alert.onDidDismiss();
+      const values = data.data.values;
+      return [values[0], values[1]];
+    } catch {
+      return null;
+    }
+  }
+
   async presentToast(message: string, duration: number=1250, position: 'top'|'bottom'|'middle'='bottom') {
     const toast = await this.toastController.create({
       message: message,
@@ -58,18 +81,35 @@ export class Tab1Page {
     await toast.present();
   }
 
+  processQuantityInput(quantity: any) {
+    quantity = Math.abs(Number(quantity));
+    if (!quantity) quantity = 1;
+    return quantity;
+  }
+
   async addItem() {
     const result = await this.presentAddBox();
 
     if (result) {
       let [name, quantity] = result;
-      quantity = Math.abs(Number(quantity));
-      if (!quantity) quantity = 1;
+      quantity = this.processQuantityInput(quantity);      
 
       const newItem = new GroceryItem(name, quantity);
       this.items.push(newItem);
       this.presentToast('Grocery item added successfully!');
     }
+  }
+
+  async editItem(item: GroceryItem) {
+    const result = await this.presentEditBox(item);
+
+    if (result) {
+      let [name, quantity] = result;
+      quantity = this.processQuantityInput(quantity);
+      
+      item.name = name;
+      item.quantity = quantity;
+    } 
   }
 
   removeItem(i: number) {
