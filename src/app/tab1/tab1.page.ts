1006d92cbdb5b922c2e5c4f9adb9506c2f42ffa5
bwilkins96 <ben.jr.speed@gmail.com>
2023-07-19 13:14:31 -0400

    add grocery item

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,54 @@
-import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { Component, numberAttribute } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule, AlertController } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+
+class GroceryItem {
+  name: string;
+  quantity: number;
+
+  constructor(name: string, quantity: number) {
+    this.name = name;
+    this.quantity = quantity;
+  }
+}
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent],
+  imports: [IonicModule, ExploreContainerComponent, CommonModule],
 })
 export class Tab1Page {
-  constructor() {}
+  items: Array<GroceryItem> = [];
+
+  constructor(private alertController: AlertController) {}
+
+  async presentAddBox() {
+    const inputs = [
+      {placeholder: 'Item Name'},
+      {placeholder: 'Quantity'}
+    ]
+
+    const alert = await this.alertController.create({
+      header: 'Add Grocery Item',
+      inputs: inputs,
+      buttons: ['Add Item']
+    });
+
+    await alert.present();
+    const data = await alert.onDidDismiss();
+    const values = data.data.values;
+
+    return [values[0], values[1]]
+  }
+
+  async addItem() {
+    const [name, quantity] = await this.presentAddBox();
+    const newItem = new GroceryItem(name, Number(quantity));
+    this.items.push(newItem);
+
+    console.log(this.items);
+  }
 }
